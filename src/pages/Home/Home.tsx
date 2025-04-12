1f4fe2117eaaa896f57e39deb6a5f7c392356acf
Vansh <vanshmahajan2001@gmail.com>
2025-04-12 15:04:16 +0530

    Logout changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,6 @@ import MoodHistoryPanel from '../../components/MoodHistoryPanel';
 import MirrorCard from '../../components/MirrorCard';
 import MannKiBaatCard from '../../components/MannKiBaatCard';
 import CalendarStreakWidget from '../../components/Calendar';
-import LogoutButton from '../../components/LogoutButton';
 import { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router';
@@ -17,13 +16,7 @@ function Home() {
   // Log user details once authentication is confirmed and user data is available
   useEffect(() => {
     if (!isLoading && isAuthenticated && user) {
-      console.log('Auth0 User Details:', user);
-      // You can access specific details like:
-      console.log('Email:', user.email);
-      console.log('Name:', user.name);
-      console.log('Sub (ID):', user.sub);
-      // Note: Available user properties depend on the scopes requested during login
-      // and the information available in the Auth0 user profile.
+      console.log('User is authenticated');
     } else if (!isLoading && !isAuthenticated) {
       console.log('User is not authenticated');
       navigate('/login'); // Redirect to login if not authenticated
@@ -37,7 +30,6 @@ function Home() {
     return (
       <div className="bg-primary text-text min-h-screen font-body">
         <Header />
-        <LogoutButton />
         <div className="h-full flex flex-col gap-1 pb-8">
           <div className="w-screen flex justify-around gap-4 px-6 pt-4 min-h-fit">
             <div className="w-full rounded-lg hover:ring-1 hover:bg-primary-hover">
